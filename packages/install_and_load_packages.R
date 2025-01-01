3e35d956298018fdc59a2459f98ade34fc7609bf
andre <andreluizmtg@gmail.com>
2025-01-01 13:42:31 -0300

    1/1

--- packages/install_and_load_packages.R
+++ packages/install_and_load_packages.R
@@ -19,22 +19,6 @@ install_and_update_packages <- function(...) {
 # install_and_update_packages("janitor", "dplyr", "ggplot2", "tidyr")
 # ------------------------------------------------------------------------------
 
-# # Define a function to install and update packages
-# install_and_update_packages <- function(...) {
-#   packages <- c(...)
-#   for (pkg in packages) {
-#     if (!require(pkg, character.only = TRUE)) {
-#       install.packages(pkg, method = "wininet")
-#     }
-#     update.packages(pkg, ask = FALSE, method = "wininet")
-#   }
-# }
-
-# ------------------------------------------------------------------------------
-# Call the function with the package names
-# install_and_update_packages("janitor", "dplyr", "ggplot2", "tidyr")
-# ------------------------------------------------------------------------------
-
 packages = c(
   # Data Manipulation and Analysis
   "tidyverse", # suite of packages designed for data science
